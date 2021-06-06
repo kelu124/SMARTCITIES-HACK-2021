ee79ae5108b99f7cb246c2f1bf7aefbe2b3fc969
Fabien MM <fabien.blanchais@mottmac.com>
2021-06-06 21:27:24 +0800

    networkx_updated

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -7,34 +7,43 @@ import seaborn as sns
 import logging
 st.write(nx.__version__)
 
+import geopandas
+from geopy.geocoders import Nominatim
+#import utm
+from scipy.spatial.distance import cdist
+
+
 # Writing the sidebar
-st.sidebar.write("### Parameters")
 
-start_point = st.sidebar.text_input('Choose starting point...',"(383474.64370036806,146569.12470285365)") 
-end_point = st.sidebar.text_input('Choose destination...',"(383423.34972914157,145768.7813587437)") 
-security_check = st.sidebar.checkbox('Security', value=True)
-perf_cctv = st.sidebar.slider ( "CCTV" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
-perf_lamps = st.sidebar.slider ( "Lamps" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
-security_check = st.sidebar.checkbox('tourist path', value=False)
+st.sidebar.radio('', ['Walking','Running', 'Cycling'])
 
-#start=(383474.64370036806,146569.12470285365)
-#end=(383423.34972914157,145768.7813587437)
+st.sidebar.radio('Are you...', ['Local',' Tourist (we optimise your route to help you discover places popular with tourists)'])
 
-if start_point:
-    start = start_point
-if end_point:
-    end = end_point
-if security_check:
-    sec=1
-    if perf_cctv:
-        cctv=perf_cctv
-    if perf_lamps:
-        lamps=perf_lamps
-else:
-    sec=0
-    cctv=0
-    lamps=0
-    
+
+start_point = st.sidebar.text_input('Choose starting point...',"Marina Bay, Singapore") 
+end_point = st.sidebar.text_input('Choose destination...',"Boat Quay, Singapore") 
+
+security = st.sidebar.checkbox('Security',value = True)
+if security:
+    cctv_perf = st.sidebar.slider ( "CCTV" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    lamps_perf = st.sidebar.slider ( "Lamps" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    trafic_perf = st.sidebar.slider ( "Trafic" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    pop_perf = st.sidebar.slider ( "Population density" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+
+if st.sidebar.checkbox('Amenities'):
+    st.sidebar.slider ( "Public toilets" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Water fountain" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+if st.sidebar.checkbox('Food'):
+    st.sidebar.slider ( "Bubble tea " , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Hawker centres" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Cafes" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Bars and pubs" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+if st.sidebar.checkbox('Attractions'):
+    st.sidebar.slider ( "Shopping centers " , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Museums" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Parks" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    st.sidebar.slider ( "Events" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+
 
 @st.cache(allow_output_mutation=True)#allow_output_mutation=True)
 def loadShp(path):
@@ -51,11 +60,12 @@ def get_weighted_graph(G):
         data=data
         coord1 = data[0]
         coord2 = data[1]
-        data[2]['weight']=(data[2]['CCTV50mRE']*sec*cctv)+(data[2]['Lamps50m']*sec*lamps)+data[2]['length']
-        try:
+        if security:
+            data[2]['weight']=(data[2]['CCTV50mRE']*cctv_perf)+(data[2]['Lamps50m']*lamps_perf)+data[2]['Length']
             weighted_G.add_edge(coord1,coord2,weight=data[2]['weight'])
-        except:
-            weighted_G.add_edge(coord1,coord2,weight=-999)
+        else:
+            data[2]['weight']=data[2]['Length']
+            weighted_G.add_edge(coord1,coord2,weight=data[2]['weight'])
     #logging.info(f'{data[0]}')
     #logging.info(f'{data[1]}')
     #logging.info(f'{data[2]}')
@@ -63,28 +73,70 @@ def get_weighted_graph(G):
     labels = nx.get_edge_attributes(weighted_G,'weight')
     return weighted_G, pos, labels
 
-G = loadShp("../data/SingaporeStreetLampsCCTV/StreetsAndLampsAndCCTV_EPSG32648.shp")
+def closest_point(point, points):
+    """ Find closest point from a list of points. """
+    return points[cdist([point], points).argmin()]
+
+G = loadShp("C:/Users/BLA88076/repos/SMARTCITIES-HACK-2021/data/SingaporeStreets/StreetsLampsCCTVTrees.shp")
 weighted_G, pos, labels = get_weighted_graph(G)
 
-fig, ax = plt.subplots(frameon=False)
-nx.draw_networkx_nodes(weighted_G,pos, node_size=10,node_color='r')
-nx.draw_networkx_edges(weighted_G, pos)
-st.pyplot(fig)
+locator = Nominatim(user_agent="http://101.98.38.221:8501")
+if start_point:
+    location_s = locator.geocode(start_point)
+    x_start = location_s.longitude
+    y_start = location_s.latitude
+if end_point:
+    location_e = locator.geocode(end_point)
+    x_end = location_e.longitude
+    y_end = location_e.latitude
+    
+# df_nodes = pd.DataFrame(columns=['y','x'])
+# for n in pos:
+#     nxy=[n[0],n[1]]
+#     series = pd.Series(nxy, index = df_nodes.columns)
+#     df_nodes=df_nodes.append(series, ignore_index=True)
+# df_nodes.to_csv("C:/Users/BLA88076/repos/SMARTCITIES-HACK-2021/data/SingaporeStreets/SG_nodes.csv")
 
-## Finding shortest path
-if 0:
-    route=nx.shortest_path(weighted_G,source=start, target=end)
-    df = pd.DataFrame(columns=['x','y'])
-    for r in route:
-        pt=[r[0],r[1]]
-        series = pd.Series(pt, index = df.columns)
-        df=df.append(series, ignore_index=True)
+df_nodes = pd.read_csv("C:/Users/BLA88076/repos/SMARTCITIES-HACK-2021/data/SingaporeStreets/SG_nodes.csv", index_col=0)
+df_pts = pd.DataFrame([[x_start,y_start],[x_end,y_end]],columns=['x','y'])
+df_nodes['point'] = [(x, y) for x,y in zip(df_nodes['x'], df_nodes['y'])]
+df_pts['point'] = [(x, y) for x,y in zip(df_pts['x'], df_pts['y'])]
+df_pts['closest'] = [closest_point(x, list(df_nodes['point'])) for x in df_pts['point']]
 
-    fig, ax = plt.subplots(frameon=False)
-    sns.scatterplot(data=df, x='x',y='y')
-    st.pyplot(fig)
+start=df_pts.iloc[0]['closest']
+end=df_pts.iloc[1]['closest']
 
-    #nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-    plt.xlim(382400, 383300) #This changes and is problem specific
-    plt.ylim(145650, 147250) #This changes and is problem specific
-    st.pyplot(fig)
+
+route=nx.shortest_path(weighted_G,source=start, target=end)
+df = pd.DataFrame(columns=['x','y'])
+for r in route:
+    pt=[r[0],r[1]]
+    series = pd.Series(pt, index = df.columns)
+    df=df.append(series, ignore_index=True)
+
+
+df.plot.line(x='x',y='y')
+
+
+# fig, ax = plt.subplots(frameon=False)
+# nx.draw_networkx_nodes(weighted_G,pos, node_size=10,node_color='r')
+# nx.draw_networkx_edges(weighted_G, pos)
+# st.pyplot(fig)
+
+# ## Finding shortest path
+# if 0:
+#     route=nx.shortest_path(weighted_G,source=start, target=end)
+#     df = pd.DataFrame(columns=['x','y'])
+#     for r in route:
+#         pt=[r[0],r[1]]
+#         series = pd.Series(pt, index = df.columns)
+#         df=df.append(series, ignore_index=True)
+
+#     fig, ax = plt.subplots(frameon=False)
+#     sns.scatterplot(data=df, x='x',y='y')
+#     st.pyplot(fig)
+
+#     #nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+#     plt.xlim(382400, 383300) #This changes and is problem specific
+#     plt.ylim(145650, 147250) #This changes and is problem specific
+#     st.pyplot(fig)
