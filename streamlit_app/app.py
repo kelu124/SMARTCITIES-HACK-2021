0bf027f855a2f1b05adb714ef6a41553f10b3481
Owen Flanagan <owen.flanagan@mottmac.com>
2021-06-06 15:19:35 +1200

    updated streamlit app to use folium map

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,11 +1,11 @@
-import streamlit as st
+from helpers import *
+#st.write(nx.__version__)
 
-import networkx as nx
-import matplotlib.pyplot as plt 
-import pandas as pd
-import seaborn as sns
-import logging
-st.write(nx.__version__)
+#old file
+#input_file = "data/shp_input_networkx_EPSG32648/split_intersection.shp"
+#new file
+input_file = "data/StreetsAndLampsAndCCTV.shp"
+
 
 # Writing the sidebar
 st.sidebar.write("### Parameters")
@@ -13,76 +13,35 @@ perf = st.sidebar.slider ( "Green weights" , min_value=0.5 , max_value=2.0 , val
 nTirages = st.sidebar.slider (  "Lights" , min_value=5 , max_value=500 , value=10, step=5 , format=None , key=None )
 capped = st.sidebar.checkbox('Some value')
 
+# Opening path, streelamps & CCTV
+fname = 'data/SingaporeStreets.zip'
+#SLC = gpd.read_file(fname)
 
-@st.cache(allow_output_mutation=True)#allow_output_mutation=True)
-def loadShp(path):
-    G= nx.readwrite.nx_shp.read_shp(path)
-    return G
+G = loadShp(input_file)
 
-G = loadShp("data/shp_input_networkx_EPSG32648/split_intersection.shp")
+weighted_G, pos, labels = get_weighted_graph(G)
 
-test_G = loadShp("data/StreetsAndLampsAndCCTV.shp")
+#GDF = gpd.read_file(input_file)
 
+#xmin, ymin, xmax, ymax = SLC.total_bounds
+# Opening trees
+# Opening park facilities
 
+# gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+# gdf = gpd.read_file('../data/park-facilities/park-facilities-kml.kml', driver='KML')
+# gdf['lon'] = gdf['geometry'].x
+# gdf['lat'] = gdf['geometry'].y
 
+G = getNetworkAround(G, 1.2806, 103.8464, 751)
+# ox.plot_graph(G)
 
-def get_weighted_graph(G):
-    """
-    Takes a graph G as input and adds the weights
-    """
-    weighted_G = nx.Graph()
-    for data in G.edges(data=True):
-        #logging.info(f'{data}')
-        coord1 = data[0]
-        coord2 = data[1]
-        data_dict = data[2]
-        #the new shape file doesn't currently have distences so 
-        #if there are no distances we just set the weights to 1
-        try:
-            distance = data_dict['distance']
-            weighted_G.add_edge(coord1,coord2,weight=data_dict['distance'])
-        except:
-            weighted_G.add_edge(coord1,coord2,weight=1)
-    #logging.info(f'{data[0]}')
-    #logging.info(f'{data[1]}')
-    #logging.info(f'{data[2]}')
-    pos = {v:v for v in weighted_G.nodes()}
-    labels = nx.get_edge_attributes(weighted_G,'weight')
-    return weighted_G, pos, labels
+for u,v,d in G.edges(data=True):
+    #ID = d["osmid"]
+    #d['weight'] = 10 +  1.0 / d['length'] # + d['Lamps50m'] + d["CCTV100mRE"]
+    d['weight'] = 1
 
+origin_point = (1.2822526633223938, 103.84732075349544) 
+destination_point = (1.2785088771898996, 103.8413733342337)
 
-weighted_G, pos, labels = get_weighted_graph(test_G)
-
-
-#TODO convert physicla address to coordinates
-#https://towardsdatascience.com/geocode-with-python-161ec1e62b89
-
-#nx.draw_networkx_nodes(weighted_G,pos, node_size=10,node_color='r')
-
-fig, ax = plt.subplots(frameon=False)
-nx.draw_networkx_nodes(weighted_G,pos, node_size=10,node_color='r')
-nx.draw_networkx_edges(weighted_G, pos)
-st.pyplot(fig)
-
-start=(383474.64370036806,146569.12470285365)
-end=(383423.34972914157,145768.7813587437)
-
-## Finding shortest path
-if 0:
-    route=nx.shortest_path(weighted_G,source=start, target=end)
-
-    df = pd.DataFrame(columns=['x','y'])
-    for r in route:
-        pt=[r[0],r[1]]
-        series = pd.Series(pt, index = df.columns)
-        df=df.append(series, ignore_index=True)
-
-    fig, ax = plt.subplots(frameon=False)
-    sns.scatterplot(data=df, x='x',y='y')
-    st.pyplot(fig)
-
-
-    #nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-    plt.xlim(382400, 383300) #This changes and is problem specific
-    plt.ylim(145650, 147250) #This changes and is problem specific
-    st.pyplot(fig)
+#need to update to use streamlit-folium
+mapPath(G,origin_point,destination_point,WGT = 'length')
