749af0a8d5a743cc8e20ab46e387285fb1727d78
Fabien MM <fabien.blanchais@mottmac.com>
2021-06-06 12:52:31 +0800

    streamlit_shp_sliders updated

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -9,22 +9,37 @@ st.write(nx.__version__)
 
 # Writing the sidebar
 st.sidebar.write("### Parameters")
-perf = st.sidebar.slider ( "Green weights" , min_value=0.5 , max_value=2.0 , value=1.0 , step=0.1 , format=None , key=None )
-nTirages = st.sidebar.slider (  "Lights" , min_value=5 , max_value=500 , value=10, step=5 , format=None , key=None )
-capped = st.sidebar.checkbox('Some value')
 
+start_point = st.sidebar.text_input('Choose starting point...',"(383474.64370036806,146569.12470285365)") 
+end_point = st.sidebar.text_input('Choose destination...',"(383423.34972914157,145768.7813587437)") 
+security_check = st.sidebar.checkbox('Security', value=True)
+perf_cctv = st.sidebar.slider ( "CCTV" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+perf_lamps = st.sidebar.slider ( "Lamps" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+security_check = st.sidebar.checkbox('tourist path', value=False)
+
+#start=(383474.64370036806,146569.12470285365)
+#end=(383423.34972914157,145768.7813587437)
+
+if start_point:
+    start = start_point
+if end_point:
+    end = end_point
+if security_check:
+    sec=1
+    if perf_cctv:
+        cctv=perf_cctv
+    if perf_lamps:
+        lamps=perf_lamps
+else:
+    sec=0
+    cctv=0
+    lamps=0
+    
 
 @st.cache(allow_output_mutation=True)#allow_output_mutation=True)
 def loadShp(path):
     G= nx.readwrite.nx_shp.read_shp(path)
     return G
-
-G = loadShp("data/shp_input_networkx_EPSG32648/split_intersection.shp")
-
-test_G = loadShp("data/StreetsAndLampsAndCCTV.shp")
-
-
-
 
 def get_weighted_graph(G):
     """
@@ -33,16 +48,14 @@ def get_weighted_graph(G):
     weighted_G = nx.Graph()
     for data in G.edges(data=True):
         #logging.info(f'{data}')
+        data=data
         coord1 = data[0]
         coord2 = data[1]
-        data_dict = data[2]
-        #the new shape file doesn't currently have distences so 
-        #if there are no distances we just set the weights to 1
+        data[2]['weight']=(data[2]['CCTV50mRE']*sec*cctv)+(data[2]['Lamps50m']*sec*lamps)+data[2]['length']
         try:
-            distance = data_dict['distance']
-            weighted_G.add_edge(coord1,coord2,weight=data_dict['distance'])
+            weighted_G.add_edge(coord1,coord2,weight=data[2]['weight'])
         except:
-            weighted_G.add_edge(coord1,coord2,weight=1)
+            weighted_G.add_edge(coord1,coord2,weight=-999)
     #logging.info(f'{data[0]}')
     #logging.info(f'{data[1]}')
     #logging.info(f'{data[2]}')
@@ -50,27 +63,17 @@ def get_weighted_graph(G):
     labels = nx.get_edge_attributes(weighted_G,'weight')
     return weighted_G, pos, labels
 
-
-weighted_G, pos, labels = get_weighted_graph(test_G)
-
-
-#TODO convert physicla address to coordinates
-#https://towardsdatascience.com/geocode-with-python-161ec1e62b89
-
-#nx.draw_networkx_nodes(weighted_G,pos, node_size=10,node_color='r')
+G = loadShp("../data/SingaporeStreetLampsCCTV/StreetsAndLampsAndCCTV_EPSG32648.shp")
+weighted_G, pos, labels = get_weighted_graph(G)
 
 fig, ax = plt.subplots(frameon=False)
 nx.draw_networkx_nodes(weighted_G,pos, node_size=10,node_color='r')
 nx.draw_networkx_edges(weighted_G, pos)
 st.pyplot(fig)
 
-start=(383474.64370036806,146569.12470285365)
-end=(383423.34972914157,145768.7813587437)
-
 ## Finding shortest path
 if 0:
     route=nx.shortest_path(weighted_G,source=start, target=end)
-
     df = pd.DataFrame(columns=['x','y'])
     for r in route:
         pt=[r[0],r[1]]
@@ -80,7 +83,6 @@ if 0:
     fig, ax = plt.subplots(frameon=False)
     sns.scatterplot(data=df, x='x',y='y')
     st.pyplot(fig)
-
 
     #nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
     plt.xlim(382400, 383300) #This changes and is problem specific
