4d1dba3b1b5eca19bdd5bf9be208ec89decdded7
Luc Jonveaux <Luc.Jonveaux@mottmac.com>
2021-06-06 19:23:50 +0200

    App streamlined

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,71 +1,64 @@
-from helpers import *
-#st.write(nx.__version__)
+import streamlit as st
 
-#old file
-#input_file = "data/shp_input_networkx_EPSG32648/split_intersection.shp"
-#new file
-#input_file = "data/StreetsAndLampsAndCCTV.shp"
+import networkx as nx
+import matplotlib.pyplot as plt 
+import pandas as pd
+import seaborn as sns
+import logging
+import numpy as np
+import geopandas as gpd
+from geopy.geocoders import Nominatim
+from shapely.wkt import loads
+from scipy.spatial.distance import cdist
 
-input_file = '../data/SingaporeStreetLampsCCTV/StreetsAndLampsAndCCTV_EPSG32648.shp'
+import plotly.graph_objects as go
 
-# https://towardsdatascience.com/find-and-plot-your-optimal-path-using-plotly-and-networkx-in-python-17e75387b873
+# Let's brag about digital twins ;)
+import sgp_dt as dt
+
+
+@st.cache(allow_output_mutation=True)#allow_output_mutation=True)
+def loadShp(path):
+    # Reading the overall network
+    G= nx.readwrite.nx_shp.read_shp(path)
+    # Support to network identification
+    df_nodes = pd.read_csv("../data/SG_nodes.txt", index_col=0)
+    # Some cleaning necessary for csv loadup
+    dfNodes = pd.read_csv("../data/dfNodes.csv.zip")
+    dfNodes.Pos = dfNodes.Pos.apply(lambda x: eval(x))
+    dfEdges = pd.read_csv("../data/dfEdges.csv.zip") 
+    dfEdges.geometry = dfEdges.geometry.apply(lambda x: loads(x.replace('\'', '')))
+    # Additional layers  
+    gTrees = gpd.read_file('../data/sTrees.zip') 
+    gLamps = gpd.read_file('../data/sLamps.zip') 
+    gPark = gpd.read_file('../data/sParks.zip') 
+    gCCTV = gpd.read_file('../data/sCCTV.zip') 
+    return G, df_nodes, dfNodes, dfEdges, gTrees, gLamps, gPark, gCCTV
+
+# This call should be cached
+G, df_nodes, dfNodes, dfEdges, gTrees, gLamps, gPark, gCCTV = loadShp("../data/s3/StreetsLampsCCTVTrees.shp")
 
 
 # Writing the sidebar
-st.sidebar.write("### Parameters")
-#perf = st.sidebar.slider ( "Green weights" , min_value=0.5 , max_value=2.0 , value=1.0 , step=0.1 , format=None , key=None )
-#nTirages = st.sidebar.slider (  "Lights" , min_value=5 , max_value=500 , value=10, step=5 , format=None , key=None )
-#capped = st.sidebar.checkbox('Some value')
-start_point = st.sidebar.text_input('Choose starting point...',"1.2822526633223938, 103.84732075349544") 
-end_point = st.sidebar.text_input('Choose destination...',"1.2785088771898996, 103.8413733342337") 
-security_check = st.sidebar.checkbox('Security', value=True)
-perf_cctv = st.sidebar.slider ( "CCTV" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
-perf_lamps = st.sidebar.slider ( "Lamps" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
-security_check = st.sidebar.checkbox('tourist path', value=False)
 
-#start=(383474.64370036806,146569.12470285365)
-#end=(383423.34972914157,145768.7813587437)
+UseDummyPoints= st.sidebar.checkbox('Dummy values',value = True)
+if not UseDummyPoints:
+    start_point = st.sidebar.text_input('Choose starting point...',"Marina Bay, Singapore") 
+    end_point = st.sidebar.text_input('Choose destination...',"Boat Quay, Singapore") 
 
-if start_point:
-    start = start_point
-
-if end_point:
-    end = end_point
-
-params = {}
-
-if security_check:
-    params['sec']=1
-    if perf_cctv:
-        params['cctv']=perf_cctv
-    if perf_lamps:
-        params['lamps']=perf_lamps
-else:
-    params['sec']=0
-    params['cctv']=0
-    params['lamps']=0
+#security = st.sidebar.checkbox('Security',value = True)
+security =1
+if security:
+    cctv_perf = st.sidebar.slider ( "CCTV" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None )
+    lamps_perf = st.sidebar.slider ( "Lamps" , min_value=1 , max_value=10 , value=10 , step=1 , format=None , key=None ) 
+    #@todo add type of walk, presence of trees
     
+if UseDummyPoints:
+    start,end = dt.getStartEnd("nop", "end", df_nodes, dummy= UseDummyPoints)
+else:
+    start,end = dt.getStartEnd(start_point, end_point, df_nodes, dummy= UseDummyPoints)
 
 
-# Opening path, streelamps & CCTV
-# fname = 'data/SingaporeStreets.zip'
-# SLC = gpd.read_file(fname)
-# SLC
+fig = dt.mapIt(start,end,G,dfNodes,dfEdges,SEC=security,CCTV=cctv_perf,LAMP=lamps_perf)
 
-G = loadShp(input_file)
-
-weighted_G, pos, labels = get_weighted_graph(G, params)
-
-
-G = getNetworkAround(G, 1.2806, 103.8464, 751)
-
-#need to get the coords from the boxes, problem is they 
-
-origin_point = (1.2822526633223938, 103.84732075349544) 
-destination_point = (1.2785088771898996, 103.8413733342337)
-
-start = (float(start.split(',')[0]),float(start.split(',')[1]))
-end = (float(end.split(',')[0]), float(end.split(',')[1]))
-
-#need to update to use streamlit-folium
-mapPath(G,start,end)
+st.write(fig)
---
+++ streamlit_app/sgp_dt.py
@@ -0,0 +1,242 @@
+import streamlit as st
+
+import networkx as nx
+import matplotlib.pyplot as plt 
+import pandas as pd
+import seaborn as sns
+import logging
+import geopandas
+from geopy.geocoders import Nominatim
+import numpy as np
+from scipy.spatial.distance import cdist
+import plotly.graph_objects as go
+
+#from streamlit_folium import folium_static
+#import folium
+
+from shapely.wkt import loads
+
+
+import shapely
+
+def getStartEnd(start_point,end_point,df_nodes,dummy=False):
+   
+    if dummy: #using it because i reached Nominatim limit
+        start_point = (103.855030, 1.2759796) 
+        end_point = (103.85019, 1.285865) 
+        x_end = end_point[0]
+        y_end = end_point[1]
+        x_start = start_point[0]
+        y_start = start_point[1]   
+
+    else:
+        locator = Nominatim(user_agent="http://101.98.38.221:8501")
+        if start_point:
+            location_s = locator.geocode(start_point)
+
+        if end_point:
+            location_e = locator.geocode(end_point)
+
+        x_end = location_e.longitude
+        y_end = location_e.latitude
+        x_start = location_s.longitude
+        y_start = location_s.latitude   
+                
+    df_pts = pd.DataFrame([[x_start,y_start],[x_end,y_end]],columns=['x','y'])
+    df_nodes['point'] = [(x, y) for x,y in zip(df_nodes['x'], df_nodes['y'])]
+    df_pts['point'] = [(x, y) for x,y in zip(df_pts['x'], df_pts['y'])]
+    df_pts['closest'] = [closest_point(x, list(df_nodes['point'])) for x in df_pts['point']]
+    
+    start=df_pts.iloc[0]['closest']
+    end=df_pts.iloc[1]['closest']
+    #st.write("#Closest",df_pts)
+    #st.write("INIT",start_point,end_point)
+    #st.write("APRES",start,end)
+    return start,end
+
+
+
+
+def getLL(gdf):
+    # Used for plotting the real lines from a path
+    lats, lons = [], []
+    for feature in gdf.geometry:
+        if isinstance(feature, shapely.geometry.linestring.LineString):
+            linestrings = [feature]
+        elif isinstance(feature, shapely.geometry.multilinestring.MultiLineString):
+            linestrings = feature.geoms
+        else:
+            continue
+        for linestring in linestrings:
+            x, y = linestring.xy
+            lats = np.append(lats, y)
+            lons = np.append(lons, x)
+            lats = np.append(lats, None)
+            lons = np.append(lons, None)
+    return lats,lons
+
+
+
+
+def addshortest(fig, shortest):
+
+    LatShort,LonShort = [],[]
+    for r in shortest:
+        pt=[r[0],r[1]]
+        LatShort.append(r[1])
+        LonShort.append(r[0]) 
+
+    fig.add_trace(go.Scattermapbox(
+        name = "Path",
+        mode = "lines",
+        lon = LonShort,
+        lat = LatShort,
+        marker = {'size': 10},
+        line = dict(width = 2.5, color = 'green')))
+
+    return fig
+
+
+
+def mapIt(start,end,G,dfNodes,dfEdges,SEC=0,CCTV=5.0,LAMP=5.0):
+    
+    # Creating the weighted network based on the user parameters
+    weighted_G, pos, labels = get_weighted_graph(G,SEC,CCTV,LAMP)
+    #st.write(start,end)
+    # Now that we know these nodes, we can find the shorted path
+    try:
+        route    = nx.shortest_path(weighted_G ,source=start, target=end)
+        shortest = nx.shortest_path(weighted_G ,source=start, target=end, weight = "Length")
+    except:
+        st.write("## !! Address not found")
+        return ""
+    # And getting the list of the nodes position tuples
+    lat,lon = [],[]
+    for r in route:
+        pt=[r[0],r[1]]
+        lat.append(r[1])
+        lon.append(r[0]) 
+    #st.write(route)
+    # Now that we know the latlon of the nodes, we can find the polylines linking them
+    nodes, lines = [], []
+    #st.write(dfNodes.head(3))
+    for r in route:
+        NODE = dfNodes[dfNodes.Pos == r]
+        if len(NODE):
+            nodes.append(NODE.iloc[0].ID)
+
+    #st.write(nodes)
+    for k in range(len(nodes)-1):
+        R = dfEdges[((dfEdges.TO == nodes[k]) & (dfEdges.FROM == nodes[k+1])) | ((dfEdges.TO == nodes[k+1]) & (dfEdges.FROM == nodes[k]))]
+        if len(R):
+            lines.append(R)
+    df = pd.concat(lines)
+    #df["geometry"] = df["geometry"].apply(lambda x: loads(x.replace('\'', '')) )
+    gdf = geopandas.GeoDataFrame(df, geometry='geometry')
+
+    # Starting the plot
+    fig = plot_path(gdf, lat, lon, start, end)
+    fig = addshortest(fig, shortest)
+    return fig
+
+
+
+
+
+
+
+def get_weighted_graph(G,security=0,cctv_perf=5.0,lamps_perf=5.0):
+    """
+    Takes a graph G as input and adds the weights
+    """
+    weighted_G = nx.Graph()
+    for data in G.edges(data=True):
+        data=data
+        coord1 = data[0]
+        coord2 = data[1]
+        if security:
+            # ZE formula to tweak
+            data[2]['weight']=(data[2]['CCTV50mRE']*cctv_perf)+(data[2]['Lamps50m']*lamps_perf)+data[2]['Length']
+            weighted_G.add_edge(coord1,coord2,weight=data[2]['weight'])
+        else:
+            data[2]['weight']=data[2]['Length']
+            weighted_G.add_edge(coord1,coord2,weight=data[2]['weight'])
+
+    pos = {v:v for v in weighted_G.nodes()}
+    labels = nx.get_edge_attributes(weighted_G,'weight')
+    return weighted_G, pos, labels
+
+
+
+def closest_point(point, points):
+    """ Find closest point from a list of points. """
+    return points[cdist([point], points).argmin()]
+
+
+
+def plot_path(gdf, lat, long, origin_point, destination_point):
+    
+    """
+    Given a list of latitudes and longitudes, origin 
+    and destination point, plots a path on a map
+    
+    Parameters
+    ----------
+    lat, long: list of latitudes and longitudes
+    origin_point, destination_point: co-ordinates of origin
+    and destination
+    Returns
+    -------
+    Nothing. Only shows the map.
+    """
+    # adding the lines joining the nodes
+    
+    LAT,LON = getLL(gdf)
+    
+    fig = go.Figure()
+     
+    
+    fig.add_trace(go.Scattermapbox(
+        name = "Path",
+        mode = "lines",
+        lon = long,
+        lat = lat,
+        marker = {'size': 10},
+        line = dict(width = 4.5, color = 'blue')))
+
+    fig.add_trace(go.Scattermapbox(
+        name = "Path",
+        mode = "lines",
+        lon = LON,
+        lat = LAT,
+        marker = {'size': 10},
+        line = dict(width = 4.5, color = 'red')))
+        
+    # adding source marker
+    fig.add_trace(go.Scattermapbox(
+        name = "Source",
+        mode = "markers",
+        lon = [origin_point[1]],
+        lat = [origin_point[0]],
+        marker = {'size': 12, 'color':"red"}))
+     
+    # adding destination marker
+    fig.add_trace(go.Scattermapbox(
+        name = "Destination",
+        mode = "markers",
+        lon = [destination_point[1]],
+        lat = [destination_point[0]],
+        marker = {'size': 12, 'color':'green'}))
+    
+    # getting center for plots:
+    lat_center = np.mean(lat)
+    long_center = np.mean(long)
+    # defining the layout using mapbox_style
+    fig.update_layout(mapbox_style="open-street-map",#stamen-terrain",
+        mapbox_center_lat = 30, mapbox_center_lon=-80)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+                      mapbox = {
+                          'center': {'lat': lat_center, 
+                          'lon': long_center},
+                          'zoom': 13})
+    return fig
